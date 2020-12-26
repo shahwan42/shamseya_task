60f421c0f2ad6cc2a86b8fbc06eb72b344ed15b5
Ahmed Shahwan <ahmed@shahwan.me>
2020-12-26 17:45:41 +0200

    return Reviews filtered by from_date & to_date

--- shamseya_task/core/api/serializers.py
+++ shamseya_task/core/api/serializers.py
@@ -1,8 +1,16 @@
-from shamseya_task.core.models import Review
+from shamseya_task.core.models import Answer, Review
 from rest_framework import serializers
 
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ("id",)
+
+
 class ReviewSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True)
+
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = ("id", "submitted_at", "answers")
--- shamseya_task/core/api/views.py
+++ shamseya_task/core/api/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from dateutil import parser
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,6 +9,30 @@ from shamseya_task.core.api.serializers import ReviewSerializer
 
 
 class ReviewApi(APIView):
+    queryset = Review.objects.all()
+
     def get(self, request):
-        qs = Review.objects.all()
+        from_date = request.query_params.get("from_date")
+        to_date = request.query_params.get("to_date")
+
+        qs = self.queryset
+
+        if from_date:
+            try:
+                from_date = parser.parse(from_date)
+            except parser.ParserError:
+                return Response({"detail": "Invlaid from_date"}, status=400)
+
+            qs = qs.filter(submitted_at__gte=datetime.date(from_date))
+
+        if to_date:
+            try:
+                to_date = parser.parse(to_date)
+            except parser.ParserError:
+                return Response({"detail": "Invlaid to_date"}, status=400)
+
+            qs = qs.filter(submitted_at__lte=datetime.date(to_date))
+
+        qs = qs.prefetch_related("answers")
+
         return Response(ReviewSerializer(qs, many=True).data)
