92232bc3605b491506bdbbae49b43c726aa6e958
Ahmed Shahwan <ahmed@shahwan.me>
2020-12-28 09:48:04 +0200

    arrange outputs

--- shamseya_task/core/management/commands/populate_db.py
+++ shamseya_task/core/management/commands/populate_db.py
@@ -16,33 +16,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        self.stdout.write(self.style.SUCCESS("Q1 and its choices"))
         q1_ch1 = ChoiceFactory()
         q1_ch2 = ChoiceFactory()
         q1 = QuestionFactory()
         q1.choices.set([q1_ch1, q1_ch2])
-        self.stdout.write(self.style.SUCCESS("Q1 and its choices"))
 
+        self.stdout.write(self.style.SUCCESS("Q2 and its choices"))
         q2_ch1 = ChoiceFactory()
         q2_ch2 = ChoiceFactory()
         q2 = QuestionFactory()
         q2.choices.set([q2_ch1, q2_ch2])
-        self.stdout.write(self.style.SUCCESS("Q2 and its choices"))
 
+        self.stdout.write(self.style.SUCCESS("Q3 and its choices"))
         q3_ch1 = ChoiceFactory()
         q3_ch2 = ChoiceFactory()
         q3 = QuestionFactory()
         q3.choices.set([q3_ch1, q3_ch2])
-        self.stdout.write(self.style.SUCCESS("Q3 and its choices"))
 
+        self.stdout.write(self.style.SUCCESS("Q4 and its choices"))
         q4_ch1 = ChoiceFactory()
         q4_ch2 = ChoiceFactory()
         q4 = QuestionFactory()
         q4.choices.set([q4_ch1, q4_ch2])
-        self.stdout.write(self.style.SUCCESS("Q4 and its choices"))
 
+        self.stdout.write(self.style.SUCCESS("Creating 4000 Reviews"))
         reviews = ReviewFactory.create_batch(4000)
-        self.stdout.write(self.style.SUCCESS("Created 4000 Reviews"))
 
+        self.stdout.write(
+            self.style.SUCCESS("Creating 10 Answers for each review (takes a while)")
+        )
         for review in reviews:
             self.stdout.write(
                 self.style.SUCCESS(f"Creating Answers for review {review.id}")
@@ -51,11 +54,10 @@ class Command(BaseCommand):
                 random_q = random.choice([q1, q2, q3, q4])
                 random_ch = random.choice(random_q.choices.all())
                 AnswerFactory(review=review, question=random_q, choice=random_ch)
-        self.stdout.write(self.style.SUCCESS("Created 10 Answers for each Review"))
 
         self.stdout.write(
             self.style.SUCCESS(
-                f"Now, db contains: {Review.objects.count()} Reviews, "
+                f"DB Records: {Review.objects.count()} Reviews, "
                 f"{Choice.objects.count()} Choices, "
                 f"{Question.objects.count()} Questions, and "
                 f"{Answer.objects.count()} Answers"
