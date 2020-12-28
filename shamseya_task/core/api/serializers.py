f78f4200369074b216448cb52ef0991221553440
Ahmed Shahwan <ahmed@shahwan.me>
2020-12-28 09:48:53 +0200

    better serialization

--- shamseya_task/core/api/serializers.py
+++ shamseya_task/core/api/serializers.py
@@ -5,10 +5,17 @@ from shamseya_task.core.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     answers = serializers.SerializerMethodField()
+    count = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
-        fields = ("submitted_at", "answers")
+        fields = ("submitted_at", "count", "answers")
 
     def get_answers(self, obj):
-        return obj.answers.values_list("id", flat=True)
+        return {
+            "count": obj.answers.count(),
+            "ids": obj.answers.values_list("id", flat=True),
+        }
+
+    def get_count(self, obj):
+        return Review.objects.filter(submitted_at=obj.submitted_at).count()
